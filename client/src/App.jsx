af8e193d8991e57fa3e0799b36b931b1b61fbe18
Yuvadi29 <adityatrivedi612@gmail.com>
2024-01-14 13:01:16 +0530

    Pages and Components Added for Frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Home from './pages/Home';
+import About from './pages/About';
+import FAQ from './pages/FAQ';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import ForgotPassword from './pages/Forgot-Password';
+import Upload from './pages/Upload';
+import Profile from './pages/Profile';
+import Search from './pages/Search';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/signup' element={<SignUp />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/forgot-password' element={<ForgotPassword />} />
+        <Route exact path='/upload' element={<Upload />} />
+        <Route exact path='/profile' element={<Profile />} />
+        <Route exact path='/search' element={<Search />} />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/faq' element={<FAQ />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ client/src/components/UploadNote.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UploadNote = () => {
+    return (
+        <div>UploadNote</div>
+    )
+}
+
+export default UploadNote
