3f84c61e1c6d1d905a5d059d5ca3d4858f683037
Yuvadi29 <adityatrivedi612@gmail.com>
2024-01-15 21:25:51 +0530

    Added Logo and Started with Navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <div>
+            <nav className='relative w-[100%] h-[8vh]'>
+                <img src="https://findmynotes.pythonanywhere.com/static/media/images/logo.png" alt="Logo" className='top-0 absolute object-contain bottom-0 right-0 left-0 m-auto h-[200%] ml-[20px]' />
+            </nav>
+        </div>
     )
 }
 
