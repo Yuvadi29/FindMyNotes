2933178ff7a3f30287ca285bee3dc4238e1562bf
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-19 22:46:14 +0530

    added spacing

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        heroSectionHeight: "calc(100vh - 60px)",
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
-
+  plugins: [require("@tailwindcss/forms")],
+};
