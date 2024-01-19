7f1dfbfd7e7409771f05e6817609951a71c755ba
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-19 23:26:34 +0530

    Added Changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,9 @@ export default {
       spacing: {
         heroSectionHeight: "calc(100vh - 60px)",
       },
+      backgroundImage: {
+        unsplashBgImage: "url('/img/bgImg.jpg')",
+      },
     },
   },
   plugins: [require("@tailwindcss/forms")],
