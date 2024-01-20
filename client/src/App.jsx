16fe5b6aa751c3937d1dc44f1b35c6fbad191abe
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-20 16:53:55 +0530

    Added navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
-import Home from './pages/Home';
-import About from './pages/About';
-import FAQ from './pages/FAQ';
-import SignUp from './pages/SignUp';
-import Login from './pages/Login';
-import ForgotPassword from './pages/Forgot-Password';
-import Upload from './pages/Upload';
-import Profile from './pages/Profile';
-import Search from './pages/Search';
+import React from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import FAQ from "./pages/FAQ";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import ForgotPassword from "./pages/Forgot-Password";
+import Upload from "./pages/Upload";
+import Profile from "./pages/Profile";
+import Search from "./pages/Search";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/signup' element={<SignUp />} />
-        <Route exact path='/login' element={<Login />} />
-        <Route exact path='/forgot-password' element={<ForgotPassword />} />
-        <Route exact path='/upload' element={<Upload />} />
-        <Route exact path='/profile' element={<Profile />} />
-        <Route exact path='/search' element={<Search />} />
-        <Route exact path='/about' element={<About />} />
-        <Route exact path='/faq' element={<FAQ />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/signup" element={<SignUp />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/forgot-password" element={<ForgotPassword />} />
+        <Route exact path="/upload" element={<Upload />} />
+        <Route exact path="/profile" element={<Profile />} />
+        <Route exact path="/search" element={<Search />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/faq" element={<FAQ />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
