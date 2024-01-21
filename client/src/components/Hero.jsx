c80c556733ea75e0be53601e881a599dc373aa68
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-21 16:12:19 +0530

    linked button to search page

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
 
   return (
-    <section className="h-heightWithoutNavbar relative flex items-center justify-center border border-black bg-unsplashBgImage bg-cover bg-center py-16 text-white">
+    <section className="relative flex h-heightWithoutNavbar items-center justify-center border border-black bg-unsplashBgImage bg-cover bg-center py-16 text-white">
       <div className="absolute inset-0 bg-black bg-opacity-70" />
       <div className="z-10 mx-auto text-center">
         <h1 className="mb-4 text-4xl font-black uppercase leading-tight md:text-5xl lg:text-6xl">
@@ -22,7 +23,12 @@ const Hero = () => {
         <div className="flex justify-center">
           {isAuthenticated ? (
             <>
-              <button className="mr-10 rounded-xl bg-white px-6 py-3 text-lg font-bold text-blue-500 hover:bg-gray-100">Get Started</button>
+              <Link
+                to="/search"
+                className="mr-10 rounded-xl bg-white px-6 py-3 text-lg font-bold text-blue-500 hover:bg-gray-100"
+              >
+                Get Started
+              </Link>
             </>
           ) : (
             <>
